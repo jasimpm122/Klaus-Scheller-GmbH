dab72036927d0758e4bbe1a1e35bff1d27bb2713
hrithik-at-105410518218 <hrithik.pramod@elogosoft.in>
2024-10-23 11:44:56 +0530

    added data

--- src/components/MockData.js
+++ src/components/MockData.js
@@ -81,7 +81,7 @@ export  const cars = [
     
     {
       imageUrl: 'https://www.bmw-scheller.de/shared/Images/neuwagen/modell-finden/g81_micro-Kopie.webp?__scale=w:585,h:329,t:2',
-      title: 'BMW 3er Touring M Automobile.',
+      title: 'BMW 3 Series Sedan M automobiles.',
       description: (
         <>
           <p>Die BMW 3er Touring M Modelle vereinen sportliche Leistungsbereitschaft mit charakterstarkem Design und hoher Alltagstauglichkeit. Ein Touring, der ein Ausrufezeichen setzt. Mit sportlicher und komfortabler M Performance, eigenständigem Design und vielen markanten Highlights im Exterieur. Innen intensivieren M Sportsitze und das M Lederlenkrad die Sportlichkeit. </p>
@@ -106,27 +106,18 @@ export  const cars = [
     },
 
     {
-      imageUrl: 'https://www.bmw-scheller.de/shared/Images/neuwagen/modell-finden/g82_cs_24.webp?__scale=w:585,h:329,t:2',
-      title: 'BMW 3er Touring M Automobile.',
+      imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/bmw_ix_m60_890x501px.jpg_340131214.jpg',
+      title: 'BMW iX M60.',
       description: (
         <>
-          <p>Die BMW 3er Touring M Modelle vereinen sportliche Leistungsbereitschaft mit charakterstarkem Design und hoher Alltagstauglichkeit. Ein Touring, der ein Ausrufezeichen setzt. Mit sportlicher und komfortabler M Performance, eigenständigem Design und vielen markanten Highlights im Exterieur. Innen intensivieren M Sportsitze und das M Lederlenkrad die Sportlichkeit. </p>
-          <p>BMW M3 Competition Touring mit M xDrive:
-            Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
-            Energieverbrauch kombiniert in l/100 km: 10,4;
-            CO2-Emissionen kombiniert in g/km: 235;
-            CO2-Klasse(n): G
+          <p>Start frei für den BMW iX M60, das erste rein elektrische SAV der BMW M GmbH: Mit einer Spitzenleistung von 455 kW (619 PS)** und einer M spezifischen Fahrwerksabstimmung bereichert er die lokal emissionsfreie Mobilität um ein überragendes Performance-Erlebnis. Mit seinen technologischen Highlights, die darüber hinaus die Bereiche automatisiertes Fahren, Bedienung und Vernetzung umfassen, repräsentiert der BMW iX M60 ein neues Verständnis von fortschrittlichem Luxus und Hochleistung: The Ultimate Electric Driving Machine.
 
-            BMW M340i xDrive Touring: 
-            Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
-            Energieverbrauch kombiniert in l/100 km: 8,7;
-            CO2-Emissionen kombiniert in g/km: 197;
-            CO2-Klasse(n): G
-            BMW M340d xDrive Touring: 
-            Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
-            Energieverbrauch kombiniert in l/100 km: 6,6;
-            CO2-Emissionen kombiniert in g/km: 173;
-            CO2-Klasse(n): G</p>
+BMW iX M60:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in kWh/100 km: 24,7;
+CO2-Emissionen kombiniert in g/km: 0;
+CO2-Klasse(n): A
+WLTP Reichweite in km: 500</p>
         </>
       )
     },
@@ -186,4 +177,309 @@ export  const cars = [
         </>
         )
       },
+      {
+        imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/bmw-8er-coupe-m-automobile-modell-finden-890x501.jpg_1694188007.jpg',
+        title: 'BMW 8er Coupé M Automobile.',
+        description: (
+          <>
+            <p>Die BMW 8er Coupé M Automobile verkörpern den Gipfel luxuriöser Fahrfreude. Auf eleganteste Weise kombinieren sie pure M Gene mit höchster Exklusivität – für sportlich-exklusive Fahrerlebnisse auf der Straße wie auf der Rennstrecke. Und das in zwei mitreißenden Varianten: Das BMW M850i xDrive Coupé besticht durch eine einzigartige Synthese aus Komfort, Leistung und Effizienz. Den Höhepunkt bildet das BMW M8 Competition Coupé mit M xDrive als noch leistungsstärkere und extravagantere Ausführung dieses High-Performance Sportwagens.
+
+BMW M850i xDrive Coupé:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 10,8;
+CO2-Emissionen kombiniert in g/km: 245;
+CO2-Klasse(n): G
+
+BMW M8 Competition Coupé:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 11,4;
+CO2-Emissionen kombiniert in g/km: 260;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+      {
+        imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/bmw-8er-cabrio-m-890x501px.jpg_1694188007.jpg',
+        title: 'BMW 8er Cabriolet M Automobile.',
+        description: (
+          <>
+            <p>Die beeindruckend leistungsstarken BMW 8er Cabriolet M Automobile führen die Faszination des offenen Fahrens auf ein neues Level und schaffen einen neuen Standard der luxuriösen Fahrfreude. An der Spitze der Baureihe steht das BMW M8 Competition Cabriolet mit M xDrive: Dieses überragende Hochleistungs-Cabriolet bietet ein noch stärkeres Gleichgewicht zwischen komfortabler Alltagstauglichkeit und aus dem Motorsport inspirierter Leistung, einzigartigen Design-Akzenten sowie luxuriösem Interieur.
+
+BMW M850i xDrive Cabriolet:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 11,0;
+CO2-Emissionen kombiniert in g/km: 249;
+CO2-Klasse(n): G
+BMW M8 Competition Cabriolet:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 11,6;
+CO2-Emissionen kombiniert in g/km: 264;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+      {
+        imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/puma-q3-2019/bmw-8er-coupe-m-890x501px.jpg_1694188007.jpg',
+        title: 'BMW 8er Gran Coupé M Automobile.',
+        description: (
+          <>
+            <p>Die BMW 8er Gran Coupé M Automobile sind außergewöhnliche Sportwagen mit vier Türen. Sie vereinen die Ingenieurskunst von BMW M mit der Exklusivität der BMW Luxusklasse. Zwei begeisternde Varianten stehen zur Wahl: Das BMW M850i xDrive Gran Coupé bietet eine einmalige Symbiose aus Leistung, Effizienz und Komfort. Und das BMW M8 Competition Gran Coupé mit M xDrive steht als extravagantes Modell der Baureihe für überragende Leistung und puren High-Performance-Luxus.
+
+BMW M850i xDrive Gran Coupé: 
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 11,0;
+CO2-Emissionen kombiniert in g/km: 251;
+CO2-Klasse(n): G
+
+BMW M8 Competition Gran Coupé: 
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 11,6;
+CO2-Emissionen kombiniert in g/km: 265;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+      {
+        imageUrl: 'https://www.bmw-scheller.de/shared/Images/neuwagen/modell-finden/x1-m35xdrive.webp?__scale=w:585,h:329,t:2',
+        title: 'BMW X1 M35i xDRIVE.',
+        description: (
+          <>
+            <p>Ein Performance-Automobil mit unverkennbarer M Charakteristik: Der BMW X1 M35i xDrive erweitert das von spontaner Fahrfreude geprägte Fahrzeugkonzept des kompakten SAV um M typische Dynamik, Agilität und Präzision. Der kraftvolle Motor mit TwinPower Turbo Technologie, M spezifische Fahrwerkstechnik, die erstmals bei einem Performance-Modell optional auch eine M Compound Bremsanlage umfasst, und der intelligente Allradantrieb BMW xDrive sorgen für ein begeisterndes Fahrerlebnis.
+
+BMW X1 M35i xDrive:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 8,2;
+CO2-Emissionen kombiniert in g/km: 186–185;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/shared/Images/neuwagen/modell-finden/2023_BMW_X2_M_890x501.webp?__scale=w:585,h:329,t:2',
+        title: 'BMW X2 M Automobil.',
+        description: (
+          <>
+            <p>Der BMW X2 M35i präsentiert sich maximal sportlich – mit Elementen in Ceriumgrau, wie der Einfassung der BMW Doppelniere, der Spiegelkappen und der zwei Endrohrblenden. Die M Einstiegsleisten mit M35i Schriftzug auf verchromten Einlegern, die optionalen M Sportsitze mit integrierten Kopfstützen und der auf Wunsch erhältlichen M Sicherheitsgurte erinnern daran, was M bedeutet: ein extrem dynamisches Fahrerlebnis.
+
+BMW X2 M35i: 
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 8,1;
+CO2-Emissionen kombiniert in g/km: 183–182;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+      {
+        imageUrl: 'https://www.bmw-scheller.de/shared/Images/neuwagen/modell-finden/bmw-x3-m50_890x501px.webp?__scale=w:585,h:329,t:2',
+        title: 'BMW X3 M50 xDRIVE.',
+        description: (
+          <>
+            <p>Er ist der souveräne Topsportler an der Spitze des Modellprogramms: Angetrieben vom stärksten jemals in einem M Performance Automobil eingesetzten Reihen-6-Zylinder-Motor beeindruckt der neue BMW X3 M50 xDrive mit seinen einzigartigen Performance-Eigenschaften und seiner charakteristischen Ausstrahlung. Der kraftvolle Antrieb, das M Fahrwerk und die M spezifischen Designmerkmale gewährleisten genau die Kombination aus Dynamik, Agilität und Präzision, die pure Fahrfreude ausmacht.
+
+BMW X3 M50 xDrive:
+Kraftstoffverbrauch kombiniert in l/100 km 8,3–7,7;
+CO2-Emissionen kombiniert in g/km 189–175;
+CO2-Klasse G–F</p>
+        </>
+        )
+      },
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/BMW_X4_M_Automobil_890x501px.jpg_1694188007.jpg',
+        title: 'BMW X4 M Automobile.',
+        description: (
+          <>
+            <p>Rebellen mit begeisterndem Charme: Die BMW X4 M Automobile vereinen das hohe Leistungspotenzial und die Exklusivität eines M Automobils mit dem progressiven Konzept eines BMW X4. Der BMW X4 M Competition steckt voller Motorsport-DNA und bietet Fahrdynamik auf höchstem Niveau. Der BMW X4 M40i und der BMW X4 M40d präsentieren sich auf jeder Strecke in Bestform und überzeugen mit einer cleveren Symbiose aus Performance, Komfort und Effizienz.
+
+BMW X4 M Competition:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 10,8;
+CO2-Emissionen kombiniert in g/km: 247;
+CO2-Klasse(n): G
+BMW X4 M40i:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 9,5;
+CO2-Emissionen kombiniert in g/km: 215;
+CO2-Klasse(n): G
+
+BMW X4 M40d:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 7,7;
+CO2-Emissionen kombiniert in g/km: 202;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/bmw-x5m-2023.jpg_1694188007.jpg',
+        title: 'BMW X5 M Automobile.',
+        description: (
+          <>
+            <p>Überlegenheit in jeder Disziplin: Die BMW X5 M Automobile manifestieren ihren Führungsanspruch mit kraftvollen Proportionen, exklusivem M Design und herausragender Leistungsfähigkeit. Erleben Sie ein adrenalingeladenes Fahrerlebnis mit diesen extrovertierten Sports Activity Vehicles (SAV): Der BMW X5 M50i überzeuget durch die einzigartige Verbindung aus Leistung, Komfort und Effizienz. Der BMW X5 M mit M xDrive besticht mit einem luxuriösen Interieur und einer von Rennsportgenen geprägten Fahrdynamik. 
+
+BMW X5 M Competition:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 13,1;
+CO2-Emissionen kombiniert in g/km: 295;
+CO2-Klasse(n): G
+
+BMW X5 M60i xDrive: 
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 12,5;
+CO2-Emissionen kombiniert in g/km: 283;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/bmw-x6m-2023_klein.jpg_1694188007.jpg',
+        title: 'BMW X6 M Automobile.',
+        description: (
+          <>
+            <p>Überlegenheit auf jedem Untergrund: Die BMW X6 M Automobile demonstrieren ihre extrovertierte Dominanz mit herausragender Fahrdynamik, ausdrucksstarkem M Design und einer konkurrenzlosen Agilität. Entdecken Sie drei fulminante Sports Activity Coupés (SAC): Der BMW X6 M50i überzeugt durch die einzigartige Verbindung aus Leistung, Komfort und Effizienz. Besonders leistungsstark und extravagant ist der BMW X6 M Competition mit M xDrive, der mit einem luxuriösen Interieur und mit einer von Rennsportgenen geprägten Fahrdynamik besticht.
+
+BMW X6 M Competition:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 12,9;
+CO2-Emissionen kombiniert in g/km: 292;
+CO2-Klasse(n): G
+
+BMW X6 M60i xDrive:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 12,3–11,7;
+CO2-Emissionen kombiniert in g/km: 279–265;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/puma-q3-2019/bmw-m-x7m-2022-890x501px.jpg_340131214.jpg',
+        title: 'BMW X7 M Automobile.',
+        description: (
+          <>
+            <p>Mehr Präsenz, mehr Sportlichkeit, mehr Digitalisierung: Der BMW X7 M60i xDrive setzt wegweisende Impulse für souveräne Fahrfreude im Segment der Luxusautomobile. Das charakteristische Exterieur bildet mit dem leistungsstarken V8-Triebwerk mit M TwinPower Turbo Technologie eine beeindruckende Kombination. Der BMW X7 M60i xDrive bietet eine unverwechselbare Verbindung aus Exklusivität, Fahrdynamik, Raumkomfort und Vielseitigkeit.
+
+BMW X7 M60i xDrive:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 12,9;
+CO2-Emissionen kombiniert in g/km: 292;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/2022_BMW_XM_890x501px.jpg_340131214.jpg',
+        title: 'BMW XM',
+        description: (
+          <>
+            <p>Der BMW XM kombiniert mit seinem TwinPower Turbo V8-Zylinder-Benzinmotor die gewohnte High-Performance eines BMW M mit der innovativen Technik eines Plug-in-Hybrids. Mit 480 kW (653 PS) und 800 Nm Drehmoment beschleunigen Sie innerhalb von 4, 3 Sekunden* und das adaptive M Fahrwerk Professional sowie das M Sport Differenzial sorgen für optimale Leistung und hoch performante Fahrdynamik. Freuen Sie sich außerdem auf ein eindrucksvolles Exterieur und luxuriöse Innenausstattung.
+
+BMW XM:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch gewichtet kombiniert: 32,2 kWh/100 km und  1,7 l/100 km
+CO2-Emissionen gewichtet kombiniert in g/km: 37
+CO2-Klasse(n): A
+Kraftstoffverbrauch bei entladener Batterie kombiniert: 7,8 l/100 km
+Reichweite in km (EAER Reichweite): 49</p>
+        </>
+        )
+      },
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/export/shared/Images/neuwagen/modell-finden/2022_BMW_Z4M.jpg_340131214.jpg',
+        title: 'BMW Z4 M Automobil.',
+        description: (
+          <>
+            <p>Entdecken Sie einmalige Sportlichkeit mit dem BMW M Z4 M40i Roadster und lassen Sie sich vom ikonischen Design aus dem Motorsport begeistern. 250 kW (340 PS) kraftvolle Fahrdynamik und hochpräzises Handling warten auf Sie. Gleichzeitig sorgt das M Aerodynamikpaket für optimale Effizienz und Dynamik. Durch das Interieur mit M Sportsitzen und dem M Lenkrad inklusive Multifunktion und M Emblem wird das sportliche Fahrerlebnis perfekt.
+
+BMW Z4 Roadster M40i:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in l/100 km: 8,79;
+CO2-Emissionen kombiniert in g/km: 197;
+CO2-Klasse(n): G</p>
+        </>
+        )
+      },
+
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/shared/Images/neuwagen/modell-finden/i4M50_24_micro.webp?__scale=w:890,h:501,t:2',
+        title: 'BMW i4 M50.',
+        description: (
+          <>
+            <p>Der BMW XM kombiniert mit seinem TwinPower Turbo V8-Zylinder-Benzinmotor die gewohnte High-Performance eines BMW M mit der innovativen Technik eines Plug-in-Hybrids. Mit 480 kW (653 PS) und 800 Nm Drehmoment beschleunigen Sie innerhalb von 4, 3 Sekunden* und das adaptive M Fahrwerk Professional sowie das M Sport Differenzial sorgen für optimale Leistung und hoch performante Fahrdynamik. Freuen Sie sich außerdem auf ein eindrucksvolles Exterieur und luxuriöse Innenausstattung.
+
+            Elegante Sportlichkeit mit neuem Design: Der BMW i4 M50 xDrive überträgt die M typische Kombination aus Agilität, Dynamik und Präzision in die Welt der Elektromobilität – und steht dabei für besonders intensive Fahrfreude. Mit jeweils einem BMW M eDrive Elektromotor an der Vorder- und Hinterachse, die gemeinsam eine Höchstleistung von 400 kW (544 PS) erzeugen, bietet das Gran Coupé ein faszinierendes Fahrerlebnis. Und das in Verbindung mit einer Reichweite von bis zu 522 Kilometern.
+
+BMW i4 M50 xDrive:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in kWh/100 km: 22,5;
+CO2-Emissionen kombiniert in g/km: 0
+CO2-Klasse(n): A
+Reichweite in km: 415</p>
+        </>
+        )
+      },
+
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/shared/Images/neuwagen/modell-finden/bmw-i5m60-micro.webp?__scale=w:890,h:501,t:2',
+        title: 'BMW i5 M60.',
+        description: (
+          <>
+            <p>Umfangreich und durchdacht: Mit dem neuen BMW i5 M60 erwartet Sie bereits serienmäßig ein hohes Ausstattungsniveau. Häufig ausgewählte Sonderausstattungen wurden in den Serienumfang übernommen, wie beispielsweise die Adaptiven LED-Scheinwerfer, der Parking Assistent inkl. Rückfahrkamera oder der Driving Assistent inkl. Spurwechselwarnung. Zusätzlich lassen drei miteinander kombinierbare Ausstattungspakete keine Wünsche offen.
+
+BMW i5 M60 xDrive:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in kWh/100 km: 20,5;
+CO2-Emissionen kombiniert in g/km: 0;
+CO2-Klasse(n): A
+Reichweite in km: 457–458</p>
+        </>
+        )
+      },
+
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/shared/Images/neuwagen/modell-finden/i5_touring_m60.webp?__scale=w:890,h:501,t:2',
+        title: 'BMW i5 M60 xDrive Touring.',
+        description: (
+          <>
+            <p>Der neue BMW i5 M60 xDrive Touring macht bleibenden Eindruck: Sein vollelektrischer Antrieb mit 442 kW (601 PS) Leistung erzeugt dank M typisch hoher Fahrdynamik jede Menge Adrenalin, ganz ohne lokale Emissionen. Das Adaptive M Fahrwerk Professional und die Integral-Aktivlenkung optimieren Straßenlage und Wendigkeit. Seine charakterstarke Optik überzeugt durch funktionale Details wie der M Sportbremse in Rot. Und mit 570–1700 l Kofferraumvolumen setzt er sich auch im Alltag souverän ins Spitzenfeld. 
+
+BMW i5 M60 xDrive Touring:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:  
+Energieverbrauch kombiniert: 20,8 kWh/100 km (WLTP);
+CO2-Emissionen kombiniert: 0 g/km (WLTP);
+CO2-Klasse(n): A</p>
+        </>
+        )
+      },
+
+      {
+        imageUrl: 'https://www.bmw-scheller.de/shared/Images/neuwagen/modell-finden/BMW_i7_M70-xDrive_Micro.webp?__scale=w:890,h:501,t:2',
+        title: 'BMW i7 M70 xDRIVE.',
+        description: (
+          <>
+            <p>Das stärkste rein elektrisch angetriebene BMW M Automobil: Mit dem BMW i7 M70 xDrive steht ein vollelektrisches Performance-Modell an der Spitze der neuen BMW 7er Reihe. Das Topmodell bietet die perfekte Kombination aus Elektromobilität und Sportlichkeit und damit ein im Luxussegment einzigartiges Fahrerlebnis. Sein M spezifisch ausgelegter Elektroantrieb, bestehend aus zwei hochintegrierten Antriebseinheiten an Vorder- und Hinterachse, setzt dabei dynamische Maßstäbe.
+
+BMW i7 M70 xDrive:
+Pflichtangaben gemäß Pkw-EnVKV nach WLTP:
+Energieverbrauch kombiniert in kWh/100 km: 23,7;
+CO2-Emissionen kombiniert in g/km: 0;
+CO2-Klasse(n): A
+Reichweite in km: 490</p>
+        </>
+        )
+      },
+
  ];
